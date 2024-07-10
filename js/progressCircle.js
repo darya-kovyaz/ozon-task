b261e33d255a7e237127adca7c00076b869115b2
Kovyazina Darya <daryakovyaz@ya.ru>
2024-07-10 12:10:30 +0300

    Improving decomposition and encapsulation

--- js/progressCircle.js
+++ js/progressCircle.js
@@ -1,14 +1,18 @@
 class ProgressCircle {
-    constructor(containerId, circleSelector, fillSelector) {
-        this.progressContainer = document.getElementById(containerId);
-        this.progressCircle = document.getElementById(circleSelector);
-        this.progressFill = document.getElementById(fillSelector);
+    constructor(container, circle, fill) {
+        this.progressContainer = container;
+        this.progressCircle = circle;
+        this.progressFill = fill;
         this.animationInterval = null;
     }
 
-    updateProgress(value) {
+    getCircumference() {
         const radius = this.progressCircle.r.baseVal.value;
-        const circumference = 2 * Math.PI * radius;
+        return 2 * Math.PI * radius;
+    }
+
+    updateProgress(value) {
+        const circumference = this.getCircumference();
         const offset = circumference - (value / 100) * circumference;
         this.progressCircle.style.strokeDasharray = `${circumference} ${circumference}`;
         this.progressCircle.style.strokeDashoffset = offset;
@@ -16,18 +20,26 @@ class ProgressCircle {
 
     animateProgress(isAnimating) {
         if (isAnimating) {
-            this.animationInterval = setInterval(() => {
-                let currentRotation =
-                    parseFloat(this.progressFill.style.transform.replace("rotate(", "").replace("deg)", "")) || 270;
-                if (currentRotation == 360) {
-                    currentRotation = 0;
-                }
-                this.progressFill.style.transform = `rotate(${currentRotation + 1}deg)`;
-            }, 10);
+            this.startAnimationInterval();
         } else {
-            clearInterval(this.animationInterval);
-            this.progressFill.style.transform = "rotate(270deg)";
+            this.stopAnimationInterval();
         }
+    }
+
+    startAnimationInterval() {
+        this.animationInterval = setInterval(() => {
+            let currentRotation =
+                parseFloat(this.progressFill.style.transform.replace("rotate(", "").replace("deg)", "")) || 270;
+            if (currentRotation == 360) {
+                currentRotation = 0;
+            }
+            this.progressFill.style.transform = `rotate(${currentRotation + 1}deg)`;
+        }, 10);
+    }
+
+    stopAnimationInterval() {
+        clearInterval(this.animationInterval);
+        this.progressFill.style.transform = "rotate(270deg)";
     }
 
     hideProgress(isHidden) {
@@ -54,3 +66,5 @@ class ProgressCircle {
         this.hideProgress(true);
     }
 }
+
+export default ProgressCircle;
